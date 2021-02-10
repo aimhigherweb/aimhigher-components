19507d9ae1b1e777a7eeee5414a2b614998976e0
Amy Kapernick <amy@aimhigherweb.design>
2021-02-10 16:59:50 +0800

    updated global styles

--- parts/forms/checkbox/index.js
+++ parts/forms/checkbox/index.js
@@ -1,18 +1,18 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react';
 
-import Label from '../label'
+import Label from '../label';
 
-import styles from './checkbox.module.scss'
+import styles from './checkbox.module.scss';
 
-const Checkbox = ({children, ...attr}) => {
-	attr.id = attr.id || attr.name
+const Checkbox = ({ children, ...attr }) => {
+	attr.id = attr.id || attr.name;
 
 	return (
 		<Fragment>
-			<input className={`${styles.input} ${styles[`input_${attr.type}`]}`} {...attr} />
+			<input type="checkbox" className={`${styles.input} ${styles[`input_${attr.type}`]}`} {...attr} />
 			<Label className={`${styles.label} ${styles[`label_${attr.type}`]}`} htmlFor={attr.id}>{children}</Label>
 		</Fragment>
-	)
-}
+	);
+};
 
-export default Checkbox
+export default Checkbox;
