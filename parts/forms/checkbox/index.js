bb9d06cb8678adad0793bbf6d3e31e4e14a63d34
Amy Kapernick <amy@aimhigherweb.design>
2021-02-10 17:12:34 +0800

    fixed checkbox

--- parts/forms/checkbox/index.js
+++ parts/forms/checkbox/index.js
@@ -1,15 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Label from '../label';
 
 import styles from './checkbox.module.scss';
 
 const Checkbox = ({ children, ...attr }) => {
+	const [checked, toggleChecked] = useState(false);
 	attr.id = attr.id || attr.name;
+	attr.type = attr.type || `checkbox`;
 
 	return (
 		<Fragment>
-			<input type="checkbox" className={`${styles.input} ${styles[`input_${attr.type}`]}`} {...attr} />
+			<input
+				className={`${styles.input} ${styles[`input_${attr.type}`]}`}
+				defaultChecked={checked}
+				onChange={() => toggleChecked(!checked)}
+				{...attr}
+			/>
 			<Label className={`${styles.label} ${styles[`label_${attr.type}`]}`} htmlFor={attr.id}>{children}</Label>
 		</Fragment>
 	);
